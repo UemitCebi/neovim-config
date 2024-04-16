c0d4861a7bcf394cf484792a7ad9dcf02f6bed0e
Ümit Cebi <uemit@xebi.media>
2024-04-16 13:59:32 +0200

    initial setup

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "c", 
+        "lua", 
+        "vim", 
+        "vimdoc", 
+        "query", 
+        "javascript", 
+        "css", 
+        "dockerfile", 
+        "go", 
+        "gomod", 
+        "gowork", 
+        "html", 
+        "json", 
+        "proto", 
+        "sql", 
+        "svelte", 
+        "tsx",
+        "typescript",
+        "yaml"
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
