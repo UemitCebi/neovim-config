bc167decc0b8faf62254ef204f850cb23459adf9
Ümit Cebi <umit@xebi.media>
2024-04-17 22:32:56 +0200

    fix harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,16 +4,16 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     require("harpoon"):setup()
+    vim.keymap.set('n', '<leader>h', function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, {})
+    vim.keymap.set('n', '<leader>ha', function() require("harpoon"):list():add() end, {})
+    vim.keymap.set('n', '<leader>1', function() require("harpoon"):list():select(1) end, {})
+    vim.keymap.set('n', '<leader>2', function() require("harpoon"):list():select(2) end, {})
+    vim.keymap.set('n', '<leader>3', function() require("harpoon"):list():select(3) end, {})
+    vim.keymap.set('n', '<leader>4', function() require("harpoon"):list():select(4) end, {})
+    vim.keymap.set('n', '<leader>5', function() require("harpoon"):list():select(5) end, {})
+    vim.keymap.set('n', '<leader>6', function() require("harpoon"):list():select(6) end, {})
   end,
-  keys = {
+  -- keys = {
    -- { "<leader>a", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu", },
-    { vim.keymap.set('n', '<leader>h', function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, {}) },
-    { vim.keymap.set('n', '<leader>ha', function() require("harpoon"):list():add() end, {}) },
-    { vim.keymap.set('n', '<leader>1', function() require("harpoon"):list():select(1) end, {}) },
-    { vim.keymap.set('n', '<leader>2', function() require("harpoon"):list():select(2) end, {}) },
-    { vim.keymap.set('n', '<leader>3', function() require("harpoon"):list():select(3) end, {}) },
-    { vim.keymap.set('n', '<leader>4', function() require("harpoon"):list():select(4) end, {}) },
-    { vim.keymap.set('n', '<leader>5', function() require("harpoon"):list():select(5) end, {}) },
-    { vim.keymap.set('n', '<leader>6', function() require("harpoon"):list():select(6) end, {}) },
-  }
+  --}
 }
