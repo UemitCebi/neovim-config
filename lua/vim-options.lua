68db7efe7564873f13b5b1b5837851dd38517619
Ümit Cebi <uemit@xebi.media>
2025-02-25 20:45:46 +0100

    increase foldcolumn

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -17,7 +17,7 @@ vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 --vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
+vim.opt.foldlevelstart = 3
 vim.opt.foldnestmax = 4
 
 vim.o.updatetime = 250
