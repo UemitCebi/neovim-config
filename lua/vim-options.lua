127678c2d8c46917a8eb9688d2e454011eb12819
Ümit Cebi <uemit@xebi.media>
2025-03-10 10:54:39 +0100

    update folding settings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,11 +14,21 @@ vim.wo.number = true
 
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
---vim.opt.foldcolumn = "0"
+vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 3
+vim.opt.foldlevelstart = 99
 vim.opt.foldnestmax = 4
+
+local function close_all_folds()
+  vim.api.nvim_exec2("%foldc!", { output = false })
+end
+local function open_all_folds()
+  vim.api.nvim_exec2("%foldo!", { output = false })
+end
+
+vim.keymap.set("n", "<leader>zs", close_all_folds, { desc = "[s]hut all folds" })
+vim.keymap.set("n", "<leader>zo", open_all_folds, { desc = "[o]pen all folds" })
 
 vim.o.updatetime = 250
 
