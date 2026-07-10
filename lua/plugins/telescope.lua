7e073b59a825e3b8b116e4d70402bae4e996c8de
Ümit Cebi <ue.cebi@mehrwerk.com>
2026-07-10 11:45:38 +0200

    feat(plugins): update lazy-lock and enhance telescope and treesitter configurations

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,36 +1,41 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    cmd = "Telescope",
-    keys = {
-      { "<leader>ff",        desc = "Find files" },
-      { "<leader>fg",        desc = "Grep through files in project" },
-      { "<leader>fb",        desc = "Find open buffers" },
-      { "<leader><leader>",  desc = "Recent files" },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		cmd = "Telescope",
+		keys = {
+			{ "<leader>ff", desc = "Find files" },
+			{ "<leader>fg", desc = "Grep through files in project" },
+			{ "<leader>fb", desc = "Find open buffers" },
+			{ "<leader><leader>", desc = "Recent files" },
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
 
-      telescope.setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
+			telescope.setup({
+				defaults = {
+					preview = {
+						treesitter = false,
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-      telescope.load_extension("ui-select")
+			telescope.load_extension("ui-select")
 
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep through files in project" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find open buffers" })
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Recent files" })
-    end,
-  },
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep through files in project" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find open buffers" })
+			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Recent files" })
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,28 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+    local ts = require("nvim-treesitter")
+    ts.setup()
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function(ev)
+        local ok, lang = pcall(vim.treesitter.language.get_lang, ev.match)
+        lang = ok and lang or ev.match
+
+        if not vim.tbl_contains(ts.get_installed(), lang) then
+          if not vim.tbl_contains(ts.get_available(), lang) then
+            return
+          end
+          ts.install(lang):wait(120000)
+        end
+
+        vim.treesitter.start(ev.buf, lang)
+        vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
